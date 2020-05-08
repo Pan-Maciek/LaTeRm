b5bc9e54be0933c5bd37139bd2718ba32780a337
pan-maciek <kozieja.maciej@gmail.com>
2020-05-08 13:15:03 +0200

    scalafx

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,9 @@
-object Main extends App {
-  println("Hello, World!")
+import scalafx.application
+import scalafx.application.JFXApp
+
+object Main extends JFXApp {
+  stage = new application.JFXApp.PrimaryStage {
+    title.value = "Hello Stage"
+  }
+
 }

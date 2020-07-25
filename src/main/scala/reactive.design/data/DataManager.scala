d6026a40ad691d8192dc0779dccbdf0149c5cffd
Petroniuss <wojtyczekPatryk@gmail.com>
2020-07-25 23:58:39 +0200

    added useful note

--- src/main/scala/reactive.design/data/DataManager.scala
+++ src/main/scala/reactive.design/data/DataManager.scala
@@ -6,7 +6,7 @@ import reactive.design.parser.Action
 /**
   * Responsible for keeping state and forwarding updates to UI by returning Observable[UiUpdate]
   *
-  * Note that if it cannot handle some action for example setTitle he should simply forward it further..
+  * Implementation note -> if it cannot handle some action for example setTitle it should simply forward it further..
   * It was actually our previous design mistake, which led to spaghetti code.
   */
 object DataManager {}
